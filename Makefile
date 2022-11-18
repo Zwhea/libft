4e31fcacd0d65be3cdbe96692806bb8c1706456d
twang <twang@student.42lyon.fr>
2022-11-18 14:41:29 +0100

    final add

--- Makefile
+++ Makefile
@@ -6,15 +6,58 @@
 #    By: twang <twang@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/11/07 15:41:17 by twang             #+#    #+#              #
-#    Updated: 2022/11/18 13:44:23 by twang            ###   ########.fr        #
+#    Updated: 2022/11/18 14:38:38 by twang            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-NAME = libft.a
+NAME =				libft.a
 
-HEADERS = libft.h
-SOURCES = $(shell find . -type f -name 'ft_*.c')
-BONUS_SOURCES	= $(shell find . -type f -name 'ft_*_bonus.c')
+HEADERS =			libft.h
+SOURCES = 			ft_isalpha.c	\
+					ft_isdigit.c	\
+					ft_isalnum.c	\
+					ft_isascii.c	\
+					ft_isprint.c	\
+					ft_strlen.c 	\
+					*ft_memset.c	\
+					ft_bzero.c  	\
+					*ft_memcpy.c	\
+					*ft_memmove.c	\
+					ft_strlcpy.c 	\
+					ft_strlcat.c 	\
+					ft_toupper.c 	\
+					ft_tolower.c 	\
+					*ft_strchr.c 	\
+					*ft_strrchr.c	\
+					ft_strncmp.c	\
+					*ft_memchr.c	\
+					ft_memcmp.c		\
+					*ft_strnstr.c	\
+					ft_atoi.c		\
+					*ft_calloc.c	\
+					*ft_strdup.c	\
+					*ft_substr.c	\
+					*ft_strjoin.c	\
+					*ft_strtrim.c	\
+					**ft_split.c	\
+					*ft_itoa.c		\
+					*ft_strmapi.c	\
+					ft_striteri.c	\
+					ft_putchar_fd.c	\
+					ft_putstr_fd.c	\
+					ft_putendl_fd.c	\
+					ft_putnbr_fd.c	\
+			
+BONUS_SOURCES =	*ft_lstnew.c		\
+				ft_lstadd_front.c	\
+				ft_lstsize.c		\
+				*ft_lstlast.c		\
+				ft_lstadd_back.c	\
+				ft_lstdelone.c		\
+				ft_lstclear.c		\
+				ft_lstiter.c		\
+				*ft_lstmap.c		\
+				
 OBJECTS = $(SOURCES:.c=.o)
 BONUS_OBJECTS = $(BONUS_SOURCES:.c=.o)
 
--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,35 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: twang <twang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 20:31:57 by wangthea          #+#    #+#             */
-/*   Updated: 2022/11/18 09:00:05 by twang            ###   ########.fr       */
+/*   Updated: 2022/11/18 14:07:42 by twang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-// {
-// 	t_list	*new;
-// 	t_list	*temp_new;
-// 	t_list	*temp_lst;
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new;
+	t_list	*temp_new;
 
-// 	if (!lst)
-// 		return (NULL);
-// 	while (temp)
-// 	{
-// 		new = malloc(sizeof(t_list));
-// 		if (!new)
-// 			ft_delone(new, del);
-// 		new = new->next;
-// 		temp = temp->next;
-// 	}
-// 	return (new);
-// }
+	if (!lst)
+		return (NULL);
+	new = ft_lstnew((f(lst->content)));
+	lst = lst->next;
+	temp_new = new;
+	while (lst)
+	{
+		temp_new->next = ft_lstnew(f(lst->content));
+		if (!temp_new->next)
+		{
+			ft_lstclear(&new, del);
+			return (NULL);
+		}
+		temp_new = temp_new->next;
+		lst = lst->next;
+	}
+	return (new);
+}
 
 //ft_lstnew?
 //ft_delone or clear???
